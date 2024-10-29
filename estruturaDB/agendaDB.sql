63ca049514a05a23d0a37409d5df04be95157a47
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-10-29 10:10:37 -0300

    ADD: AGENDADB CONEXÃO SQL

---
+++ estruturaDB/agendaDB.sql
@@ -0,0 +1,11 @@
+CREATE database dbAgenda;
+
+USE dbAgenda;
+
+CREATE TABLE tbUsuarios (
+	nome VARCHAR(60) NOT NULL,
+    usuario VARCHAR(20) PRIMARY KEY,
+    usuario VARCHAR(20) PRIMARY KEY,
+    telefone VARCHAR(15),
+    senha VARCHAR(20) NOT NULL
+    );
