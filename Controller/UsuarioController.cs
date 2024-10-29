408c9c27ffa60a38840b88ad5bd271ef5a9cfa22
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-10-29 14:50:43 -0300

    ADD: programação do sql e banco de dados, finalizações, etc...

--- Controller/UsuarioController.cs
+++ Controller/UsuarioController.cs
@@ -11,8 +11,32 @@ namespace ProjetoAgenda.Controller
     internal class UsuarioController
     {
         public bool AddUsuario(string nome, string usuario, string telefone, string senha)
+        {
+            MySqlConnection conexao =  ConexaoDB.CriarConexao();
+
+            string sql = "INSERT INTO tbUsuarios(nome, usuario, telefone, senha) VALUES (@nome, @usuario, @telefone, @senha);";
+
+            conexao.Open();
+
+            MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+            comando.Parameters.AddWithValue("@nome", nome);
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            comando.Parameters.AddWithValue("@telefone", telefone);
+            comando.Parameters.AddWithValue("@senha", senha);
+
+            int linhasAfetadas = comando.ExecuteNonQuery();
+
+            conexao.Close();
+
+            if (linhasAfetadas > 0)
             {
-                MySqlConnection conexao =  ConexaoDB.CriarConexao();
+                return true;
             }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
