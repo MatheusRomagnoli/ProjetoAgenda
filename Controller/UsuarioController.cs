d14719a3fe3afc93643891644cb7d0c057cfb87e
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-10-29 11:30:59 -0300

    finalizações programação

---
+++ Controller/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using MySql.Data.MySqlClient;
+using ProjetoAgenda.data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAgenda.Controller
+{
+    internal class UsuarioController
+    {
+        public bool AddUsuario(string nome, string usuario, string telefone, string senha)
+            {
+                MySqlConnection conexao =  ConexaoDB.CriarConexao();
+            }
+    }
+}
