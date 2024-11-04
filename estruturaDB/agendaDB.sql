60d81cc087a0abc20055e200bee1e3fec65cb279
Matheus Romagnoli <matheuszothavio@gmail.com>
2024-11-04 08:15:13 -0300

    MODIFY: arrumando codigo SQL - agendaDB

--- estruturaDB/agendaDB.sql
+++ estruturaDB/agendaDB.sql
@@ -5,7 +5,6 @@ USE dbAgenda;
 CREATE TABLE tbUsuarios (
 	nome VARCHAR(60) NOT NULL,
     usuario VARCHAR(20) PRIMARY KEY,
-    usuario VARCHAR(20) PRIMARY KEY,
     telefone VARCHAR(15),
     senha VARCHAR(20) NOT NULL
     );
